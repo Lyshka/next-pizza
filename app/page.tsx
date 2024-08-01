6ef009373e09fdb33556ded07060c65be7e625c2
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-08-01 16:16:49 +0300

    add new components and configure project
    add shadcn components, change images config, add components: categories, filter, header and more changed shadcn components and new create custom

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  Container,
+  Filters,
+  ProductCard,
+  Title,
+  TopBar,
+} from "@/components/shared";
 
 export default function Home() {
-  return <Button variant={"outline"}>Hello</Button>
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+
+      <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              <ProductCard
+                id={0}
+                name="Мега Мясная"
+                price={550}
+                imageUrl="https://media.dodostatic.net/image/r:292x292/11EF38541E76455F9A3150F1EFDA0055.avif"
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
